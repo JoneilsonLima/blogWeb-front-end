54920b317541dadac1d99f256dceb224c235a253
joneilson pereira <joneilson84@gmail.com>
2024-03-14 14:55:45 -0300

    feat: adicionando busca por id

--- src/app/pages/view-post/view-post.component.ts
+++ src/app/pages/view-post/view-post.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { PostService } from '../../service/post.service';
+import { ActivatedRoute, Params } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Subscription } from 'rxjs';
+import { Post } from '../../models/post.model';
 
 @Component({
   selector: 'app-view-post',
   templateUrl: './view-post.component.html',
-  styleUrl: './view-post.component.scss'
+  styleUrl: './view-post.component.scss',
 })
-export class ViewPostComponent {
+export class ViewPostComponent implements OnInit, OnDestroy {
+  public postId!: number;
+  public postSubscription!: Subscription;
 
+  constructor(
+    private service: PostService,
+    private route: ActivatedRoute,
+    private snackBar: MatSnackBar
+  ) {
+    this.route.params.subscribe((resp: Params) => {
+      this.postId = +resp['id'];
+    });
+  }
+
+  ngOnInit(): void {
+    this.getPostById();
+  }
+
+  ngOnDestroy(): void {
+    this.postSubscription.unsubscribe();
+  }
+
+  getPostById(): void {
+    this.postSubscription = this.service.getPostById(this.postId).subscribe({
+      next: (post: Post) => {
+        console.log(post);
+      },
+      error: (err) => {
+        this.snackBar.open('Algo deu errado!!!', 'Ok');
+      },
+    });
+  }
 }
