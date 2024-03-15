181b074ba6d8cfe104187bc3e9e832d59b5cc50b
joneilson pereira <joneilson84@gmail.com>
2024-03-15 19:33:14 -0300

    feat: adicionando funcionalidade para buscar post por nome

--- src/app/pages/view-all/view-all.component.ts
+++ src/app/pages/view-all/view-all.component.ts
@@ -2,8 +2,9 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { PostService } from '../../service/post.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Post } from '../../models/post.model';
-import { Subscription } from 'rxjs';
+import { Subscription, debounceTime, distinctUntilChanged } from 'rxjs';
 import { Router } from '@angular/router';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-view-all',
@@ -14,15 +15,20 @@ export class ViewAllComponent implements OnInit, OnDestroy {
   public allPosts: Post[] = [];
   public allPostSubscription!: Subscription;
   public loading: boolean = false;
+  public formGroup!: FormGroup;
+  public postNotFound: boolean = false;
 
   constructor(
     private postService: PostService,
     private snackBar: MatSnackBar,
-    private router: Router
+    private router: Router,
+    private formBuilder: FormBuilder
   ) {}
 
   ngOnInit(): void {
     this.getAllPosts();
+    this._buildForm();
+    this.onClickSearchByName();
   }
 
   ngOnDestroy(): void {
@@ -45,12 +51,49 @@ export class ViewAllComponent implements OnInit, OnDestroy {
       complete: () => {
         setTimeout(() => {
           this.loading = false;
-        }, 300)
-      }
+        }, 300);
+      },
     });
   }
 
   onClickLikePost(): void {
     this.getAllPosts(false);
+  }
+
+  onClickSearchByName(): void {
+    this.formGroup
+      .get('nameSearch')
+      ?.valueChanges.pipe(distinctUntilChanged(), debounceTime(300))
+      .subscribe((name: string) => {
+        if (name) {
+          this.searchPost(name);
+        } else {
+          this.getAllPosts();
+        }
+
+      });
+  }
+
+  searchPost(name: string): void {
+    this.postService.searchPostByName(name).subscribe({
+      next: (posts: Post[]) => {
+        this.loading = true;
+        setTimeout(() => {
+          this.loading = false;
+        }, 300);
+        this.allPosts = posts;
+
+        this.postNotFound = posts.length ? false : true;
+      },
+      error: (err) => {
+        this.snackBar.open('Algo deu errado!!!', 'Ok');
+      },
+    });
+  }
+
+  private _buildForm(): void {
+    this.formGroup = this.formBuilder.group({
+      nameSearch: [null],
+    });
   }
 }
